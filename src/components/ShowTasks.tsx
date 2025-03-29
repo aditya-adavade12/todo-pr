d31e097ca63c0dc6a1078317626a3eec2d9aa4e2
Aditya Adavade <adityawork118@gmail.com>
2025-03-29 18:02:05 +0530

    Updated

--- src/components/ShowTasks.tsx
+++ src/components/ShowTasks.tsx
@@ -10,61 +10,66 @@ const ShowTasks: React.FC = () => {
     }
     const [Task, setTask] = useState<taskType[]>(JSON.parse(localStorage.getItem("Tasks") || "[]"));
     // Complete task
-    const completeTask = (e: any) => {
-        console.log("Matched");
-        setTask((prev) =>
-            prev.map(task =>
-                task.id === e.id
-                    ? { ...task, status: task.status === "incomplete" ? "complete" : "incomplete" }
-                    : task
-            )
-        );
-        console.log(Task);
-        localStorage.setItem("Tasks", JSON.stringify(Task));
-}
-// Load data from localstorage
-useEffect(() => {
-    const storedTasks = JSON.parse(localStorage.getItem("Tasks") || "[]");
-    if (storedTasks) {
-        setTask(storedTasks);
+    const completeTask = (id: any) => {
+        let getData = JSON.parse(localStorage.getItem("Tasks") || "[]");
+        const newUpdate = getData.map((item: any) => {
+            if (id === item.id) {
+                return { ...item, status: "complete" };
+            }
+            return item;
+        });
+        setTask(newUpdate);
+        localStorage.setItem("Tasks", JSON.stringify(newUpdate));
     }
-}, []);
-// Deleting the task
+    // Load data from localstorage
+    useEffect(() => {
+        const storedTasks = JSON.parse(localStorage.getItem("Tasks") || "[]");
+        if (storedTasks) {
+            setTask(storedTasks);
+        }
+    }, []);
+    // Deleting the task
+    const deleteTask = (id: number) => {
+        let getData = JSON.parse(localStorage.getItem("Tasks") || "[]");
+        const updateTasks = getData.filter((item: any) => item.id !== id);
+        console.log(updateTasks);
+        setTask(updateTasks);
+        localStorage.setItem("Tasks", JSON.stringify(updateTasks));
+    }
+    return (
+        <div className='w-[50vw] mx-auto mt-20 max-sm:w-[90vw]'>
+            {/* Users Tasks */}
+            <div id="show-tasks-container">
+                <h2 className='text-2xl font-semibold'>Your Tasks</h2>
+                {
+                    Task.length == 0 ? (
+                        <div className='text-left mt-8 text-gray-200 font-semibold'>No tasks available</div>
+                    ) : (
+                        Task.map((item) =>
 
-return (
-    <div className='w-[50vw] mx-auto mt-20 max-sm:w-[90vw]'>
-        {/* Users Tasks */}
-        <div id="show-tasks-container">
-            <h2 className='text-2xl font-semibold'>Your Tasks</h2>
-            {
-                Task.length === 0 ? (
-                    <div className='text-left mt-8 text-gray-200 font-semibold'>No tasks available</div>
-                ) : (
-                    Task.map((item) =>
-
-                        <div id="task-box" className='mt-4'>
-                            {/* Tasks */}
-                            <div id="task" className='border border-stone-700 flex flex-row items-center justify-between gap-1.5 w-full py-2 px-2 rounded-lg' key={item.id}>
-                                <div>
-                                    <p className='line-clamp-1 w-full text-wrap text-lg' ref={taskName}>{item.taskName}</p>
-                                    <p className='text-stone-500 font-semibold text-sm'>{item.timeStamp}</p>
-                                </div>
-                                <div className='flex flex-row items-center gap-1.5'>
-                                    <button onClick={() => completeTask(item.id)} className='flex text-green-500 hover:bg-stone-800 p-0.5 rounded-lg cursor-pointer'><span className="flex material-symbols-outlined">
-                                        check_circle
-                                    </span></button>
-                                    <button className='flex text-stone-300 hover:bg-stone-800 p-0.5 rounded-lg cursor-pointer'><span className="flex material-symbols-outlined">
-                                        delete
-                                    </span></button>
+                            <div id="task-box" className='mt-4'>
+                                {/* Tasks */}
+                                <div id="task" className='border border-stone-700 flex flex-row items-center justify-between gap-1.5 w-full py-2 px-2 rounded-lg' key={item.id}>
+                                    <div>
+                                        <p className='line-clamp-1 w-full text-wrap text-lg' ref={taskName}>{item.taskName}</p>
+                                        <p className='text-stone-500 font-semibold text-sm'>{item.timeStamp}</p>
+                                    </div>
+                                    <div className='flex flex-row items-center gap-1.5'>
+                                        <button onClick={() => completeTask(item.id)} className='flex text-green-500 hover:bg-stone-800 p-0.5 rounded-lg cursor-pointer'><span className="flex material-symbols-outlined">
+                                            check_circle
+                                        </span></button>
+                                        <button onClick={() => deleteTask(item.id)} className='flex text-stone-300 hover:bg-stone-800 p-0.5 rounded-lg cursor-pointer'><span className="flex material-symbols-outlined">
+                                            delete
+                                        </span></button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        )
                     )
-                )
-            }
+                }
+            </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default ShowTasks
